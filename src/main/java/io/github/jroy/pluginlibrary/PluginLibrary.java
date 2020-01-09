532c1e9a7163191891c44a4c6803172bb5679ee4
JRoy <joshroy126@gmail.com>
2020-01-08 19:34:02 -0500

    Add glow enchantment

--- src/main/java/io/github/jroy/pluginlibrary/PluginLibrary.java
+++ src/main/java/io/github/jroy/pluginlibrary/PluginLibrary.java
@@ -1,8 +1,26 @@
 package io.github.jroy.pluginlibrary;
 
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentWrapper;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
+
 public class PluginLibrary extends JavaPlugin {
+
+  public static GlowEnchantment glowEnchantment;
+
+  static {
+    try {
+      Field acceptingNew = Enchantment.class.getDeclaredField("acceptingNew");
+      acceptingNew.setAccessible(true);
+      acceptingNew.set(null, true);
+      glowEnchantment = new GlowEnchantment();
+      EnchantmentWrapper.registerEnchantment(glowEnchantment);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   @Override
   public void onEnable() {
